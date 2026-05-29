099b450045cc3240bfa01c1d90020c888ccb99a6
Patrick Roeleveld <roeleveldpatrick@protonmail.com>
2026-05-29 14:01:58 +0200

    Removed ASKITE dependency

--- examples/FEM_detailed_kite.py
+++ examples/FEM_detailed_kite.py
@@ -1,12 +1,5 @@
 from pathlib import Path
 
-from kitesim import (
-    structural_kite_fem_level_2,
-    read_struc_geometry_yaml_level_2,
-)
-from kitesim.utils import (
-    load_yaml,
-)
 from kite_fem.FEMStructure import FEM_structure
 from kite_fem.Plotting import (
     plot_structure,
@@ -17,78 +10,23 @@ from kite_fem.Plotting import (
 )
 from kite_fem.Functions import relaxbridles,fix_nodes,adapt_stiffnesses
 from kite_fem.saveload import save_fem_structure,load_fem_structure
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 PROJECT_DIR = Path(__file__).resolve().parents[1]
-kite_name = "TUDELFT_V3_KITE"  
-struc_geometry_path = (
-    Path(PROJECT_DIR)
-    / "data"
-    / f"{kite_name}"
-    / "struc_geometry_all_in_surfplan.yaml"
-)
-struc_geometry = load_yaml(struc_geometry_path)
-
-(
-    # node level
-    struc_nodes,
-    m_arr,
-    struc_node_le_indices,
-    struc_node_te_indices,
-    power_tape_index,
-    steering_tape_indices,
-    pulley_node_indices,
-    canopy_sections,
-    strut_sections,
-    simplified_bridle_points,
-    # element level
-    kite_connectivity_arr,
-    bridle_connectivity_arr,
-    bridle_diameter_arr,
-    l0_arr,
-    k_arr,
-    c_arr,
-    linktype_arr,
-    pulley_line_indices,
-    pulley_line_to_other_node_pair_dict,
-) = read_struc_geometry_yaml_level_2.main(struc_geometry)
-
-config = {"is_with_initial_point_velocity": False}
-kite = structural_kite_fem_level_2.instantiate(
-    config,
-    struc_geometry,
-    struc_nodes,
-    kite_connectivity_arr,
-    l0_arr,
-    k_arr,
-    c_arr,
-    m_arr,
-    linktype_arr,
-    pulley_line_to_other_node_pair_dict,
-    canopy_sections,
-    strut_sections,
-)[0]
-
-canopy_nodes = list(set([node for section in canopy_sections + strut_sections for node in section]))
-all_sections = canopy_sections + strut_sections
-all_sections.sort(key=lambda section: section[0])
-
-
-fe = np.loadtxt(PROJECT_DIR / "data" / f"{kite_name}" / "fe_6d.csv")
+kite = load_fem_structure(PROJECT_DIR / "data" / "TUDELFT_V3_KITE" / "detailed_kite_initial.npz")
+fe = np.loadtxt(PROJECT_DIR / "data" / "TUDELFT_V3_KITE" / "fe_6d.csv")
 
 print("DOF",np.sum(kite.bc))
 print("Beam",np.size(kite.beam_elements))
 print("Springs",np.size(kite.spring_elements))
-print("Mass",np.sum(m_arr))
+
 ax1,fig1 = plot_structure(kite,plot_nodes=False,fe=fe,plot_external_forces=True,linewidth = [1,0.75,1,3.5],plot_node_numbers=False)
 ax2,fig2 = plot_structure(kite, plot_nodes=False,plot_displacements=False,solver="spsolve",e_colors = ['black', 'black', 'black', 'black'],linewidth = [1,0.75,1,3.5],plot_2d=True,plot_2d_plane="yz")
 ax3,fig3 = plot_structure_with_strain(kite)
-kite.solve(fe=fe, max_iterations=25000, tolerance=0.01, step_limit=.005, relax_init=.25, relax_min=0.00, relax_update=0.9998, k_update=1,I_stiffness=15)
+kite.solve(fe=fe, max_iterations=1000, tolerance=0.01, step_limit=.005, relax_init=.25, relax_min=0.00, relax_update=0.9998, k_update=1,I_stiffness=15)
 
-# plot_cross_sections(kite,all_sections)
 ax4,fig4 = plot_structure(kite,fe=fe,fe_magnitude=1.5, plot_residual_forces=False,plot_external_forces=True,plot_nodes=False,plot_displacements=False,solver="spsolve",linewidth = [1,0.75,1,3.5])
 ax2,fig2 = plot_structure(kite,plot_nodes=False,plot_external_forces=True,plot_displacements=False,solver="spsolve",e_colors = ['red', 'red', 'red', 'red'], linewidth = [1,0.75,1,3.5],plot_2d=True,plot_2d_plane="yz",ax=ax2,fig=fig2)
 ax5,fig5 = plot_structure_with_strain(kite)
